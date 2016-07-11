b79f70a834837faeb13ba94e3b28b428a9092c6a
Julian Thatcher <daedalus.raistlin@gmail.com>
2016-07-11 17:33:48 +1000

    Add command line processing, clean up index

--- index.js
+++ index.js
@@ -3,14 +3,56 @@
  */
 
 var fs = require('fs');
+var Getopt = require('node-getopt');
 var jorconsole = require('./lib/jorconsole');
 var JorConsole = jorconsole.jorConsole;
 
-var memorysize = 64; // memory available in mb
-
+var memorysize = 32; // memory available in mb
+var ncores = 1;
+var threading = 'webthreads';
 var useSimpleFs = false;
+var cpu = 'asm';
+var relayURL = 'https://relay.widgetry.org/';
+var networkEnabled = false;
 
-//var httpBase = "https://s-macke.github.io/jor1k/";
+
+var getopt = new Getopt([
+	['m', 'memory=ARG', 'Set memory size (default: 32)'],
+	['s', 'cpu-safe', 'Use safe CPU'],
+	['a', 'cpu-asm', 'Use assembly CPU (default)'],
+	['S', 'cpu-smp', 'Use SMP CPU (buggy)'],
+	['C', 'cpu-cores=ARG', 'SMP CPU cores (default: 2)'],
+	['v', 'vmlinux=ARG', 'Set vmlinux path (default: autodetected)'],
+	['n', 'network', 'Enable networking'],
+	['r', 'relay=ARG', 'Set networking relay (default: widgetry)'],
+	['f', 'simple-fs', 'Use simple filesystem (no games, demos, etc)'],
+	['h', 'help', 'Show this help']
+]);
+getopt.setHelp(
+	"Usage: node index.js [OPTION]\n" +
+	"JOR1k Emulator in the console\n" +
+	"\n" +
+	"[[OPTIONS]]\n" +
+	"\n"
+);
+var opt = getopt.parse(process.argv.slice(2));
+if(opt.options.help) {
+	getopt.showHelp();
+	process.exit();
+}
+
+if(opt.options['memory']) memorysize = parseInt(opt.options['memory']);
+if(opt.options['cpu-safe']) cpu = 'safe';
+if(opt.options['cpu-asm']) cpu = 'asm';
+if(opt.options['cpu-smp']) { cpu = 'smp'; ncores = 2; vmlinux = "vmlinuxsmp.bin.bz2"; }
+if(opt.options['cpu-cores']) ncores = parseInt(opt.options['cpu-cores']);
+if(opt.options['vmlinux']) vmlinux = opt.options['vmlinux'];
+if(opt.options['network']) networkEnabled = true;
+if(opt.options['relay']) relayURL = opt.options['relay'];
+if(opt.options['simple-fs']) useSimpleFs = true;
+
+if(!networkEnabled) relayURL = false;
+
 var httpBase = "file://";
 var realBase = fileBase = __dirname + "/" + "jor1k-sysroot/or1k/";
 var fileSrc  = "basefs.json";
@@ -28,23 +70,14 @@ if(!fs.existsSync(fileBase + vmlinux)) {
 	}
 }
 
-var ncores = 1;
-var threading = 'webthreads';
-// also available: webworker
-//threading = 'webworker';
-var cpu = 'safe';
-cpu = 'asm';
-
-// Not recommended: kernel fails to find root
-if(false) {
-	cpu = 'smp';
-	ncores = 2;
-	vmlinux = "vmlinuxsmp.bin.bz2";
-}
-
-var relayURL = 'https://relay.widgetry.org/';
-//relayURL = false; // disable networking
-
+console.log(
+	"Creating emulator with settings:\n" +
+	`	CPU: ${cpu}\n` +
+	`	CPU Cores: ${ncores}\n` +
+	`	Memory: ${memorysize}\n` +
+	`	Kernel: ${vmlinux}\n` +
+	`	Networking: ${relayURL}\n`
+);
 var jc = new JorConsole({
 	path: httpBase + fileBase,
 	fs: {
